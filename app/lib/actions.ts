d2063dfbe8999c40f569c95aa61a3424fc50ec5c
phayuphat.t <phayuphat118@gmail.com>
2023-10-31 02:35:24 +0700

    [add] handle error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,28 +1,71 @@
-'use server'
-import { z } from 'zod';
-import { sql } from '@vercel/postgres';
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
- 
+"use server";
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
 const InvoiceSchema = z.object({
-  id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
-  date: z.string(),
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(["pending", "paid"]),
+    date: z.string(),
 });
- 
+
 const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = Object.fromEntries(formData.entries())
-  const { customerId, amount, status } = CreateInvoice.parse(rawFormData)
-  const amountInCents = amount * 100
-  const date = new Date().toISOString().split('T')[0]
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `
-  revalidatePath('/dashboard/invoices')
-  redirect("/dashboard/invoices")
+    const rawFormData = Object.fromEntries(formData.entries());
+    const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split("T")[0];
+    try {
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch (err) {
+        return { message: err };
+    }
+
+    // note: why redirect outside because redirect is work in case error you can paste redirect inside of catch block
+    // but why i'm putting here because make code readable only !!!!
+
+    // note: revalidatePath is for make next-client re-fetch data from server
+    // because next was store prev route and pre-fetch data for caching
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get("customerId"),
+        amount: formData.get("amount"),
+        status: formData.get("status"),
+    });
+
+    const amountInCents = amount * 100;
+
+    try {
+        await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+    } catch (error) {
+        return { message: "Database Error: Failed to Update Invoice." };
+    }
+
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath("/dashboard/invoices");
+        return { message: "Deleted Invoice" };
+    } catch (error) {
+        return { message: "Database Error: Failed to Delete Invoice" };
+    }
 }
